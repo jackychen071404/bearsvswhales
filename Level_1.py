de4dee18a4a81c3c5003ccf5ef49501dde7ae48e
Baby Money <108594877+jackychen071404@users.noreply.github.com>
2023-07-03 23:31:22 -0400

    Update Level_1.py

--- Level_1.py
+++ Level_1.py
@@ -1,20 +1,22 @@
 import pygame
 import time
 import random
-
-from classwhale import Whale
-from classbear import Bear
-from classbutton import Button
-from classEmptyButton import EmptyButton
-from classbuttons import ButtonSlider
-from coinbutton import CoinButton
-from classtower import Tower
-from coin import Coin
+from object.classwhale import Whale
+from object.classbear import Bear
+from object.classbutton import Button
+from object.classEmptyButton import EmptyButton
+from object.classbuttons import ButtonSlider
+from object.coinbutton import CoinButton
+from object.classtower import Tower
+from object.coin import Coin
+from object.Victory import Victory
+from object.OK import OK
 
 #setting up
 pygame.init()
-#bg_music.play(loops=-1)
-#bg_music.set_volume(0.1)
+bg_music = pygame.mixer.Sound("GUNNA.mp3")
+bg_music.play(loops=-1)
+bg_music.set_volume(0.1)
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 32)
 window_width = 1200
@@ -23,6 +25,12 @@ window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("BEARS VS WHALES")
 BS = pygame.sprite.GroupSingle()
 BS.add(ButtonSlider())
+Vict = pygame.sprite.GroupSingle()
+Vict.add(Victory("../Vic,Def/victory.png"))
+Def = pygame.sprite.GroupSingle()
+Def.add(Victory("../Vic,Def/defeat.png"))
+Okay = pygame.sprite.GroupSingle()
+Okay.add(OK())
 
 class Viewport():
     def __init__(self):
@@ -36,23 +44,29 @@ class Wallet():
     def __init__(self):
         self.total_coins = 0
         self.max_coins = 200
+    def update(self):
+        if self.total_coins > self.max_coins:
+            self.total_coins = self.max_coins
 wallet = Wallet()
 
 class Level_01_State:
-    def __init__(self,viewport,wallet):
+    def __init__(self,viewport,wallet,running):
+        self.running = running
+        self.tutorialstart = True
+        self.tutorial = pygame.transform.scale(pygame.image.load("../Tower_BGs/tutorial.png").convert(),(window_width,window_height))
+
         self.gamestart = True
-        #self.bg_music = pygame.mixer.Sound("GUNNA.mp3")
         self.map_width = 2000
         self.map_height = 800
-        self.background_image = pygame.transform.scale(pygame.image.load("Tower_BGs/back.png").convert(), (self.map_width, self.map_height))
+        self.background_image = pygame.transform.scale(pygame.image.load("../Tower_BGs/winterbackground.png").convert(), (self.map_width, self.map_height))
         self.viewport = viewport
         #money
         self.wallet = wallet
         self.coins = pygame.sprite.Group()
         self.COIN_GENERATE_EVENT = pygame.USEREVENT + 1
         self.COIN_GENERATE_INTERVAL = 100
-        self.coin_icon = pygame.image.load("ButtonPics/moneyIcon.png")  # 125 x 30
-        self.coin_rect = pygame.image.load("ButtonPics/moneyIcon.png").get_rect(topleft=(5, 750))  # 125 x 30
+        self.coin_icon = pygame.transform.scale(pygame.image.load("../ButtonPics/moneyIcon.png"),(180,30))
+        self.coin_rect = pygame.image.load("../ButtonPics/moneyIcon.png").get_rect(topleft=(5, 750))
         #towers
         self.tower1 = pygame.sprite.GroupSingle()
         self.tower2 = pygame.sprite.GroupSingle()
@@ -76,6 +90,7 @@ class Level_01_State:
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                self.running = False
                 pygame.quit()
                 quit()
             elif event.type == self.COIN_GENERATE_EVENT:
@@ -120,11 +135,11 @@ class Level_01_State:
             self.buttons.add(EmptyButton(842, 710))
             self.buttons.add(CoinButton(5, 687))
             self.bears.add(Bear(50, clock))
-            self.tower1.add(Tower(100, 250, "Tower_BGs/cntower.png"))
-            self.tower2.add(Tower(1700, 250, "Tower_BGs/whaletower.png"))
+            self.tower1.add(Tower(100, 250, "../Tower_BGs/cntower.png"))
+            self.tower2.add(Tower(1700, 250, "../Tower_BGs/whaletower.png"))
             pygame.time.set_timer(self.COIN_GENERATE_EVENT, self.COIN_GENERATE_INTERVAL) #set timers
             pygame.time.set_timer(self.BEAR_GENERATE_EVENT, self.BEAR_GENERATE_INTERVAL)
-            self.gamestart = False 
+            self.gamestart = False
         viewport.x = max(0, min(viewport.x, self.map_width - window_width))
         viewport.y = max(0, min(viewport.y, self.map_height - window_height))
         window.blit(self.background_image, (-viewport.x, -viewport.y))
@@ -138,6 +153,7 @@ class Level_01_State:
         window.blit(self.coin_icon, self.coin_rect) #draw money
         cointext = font.render("Monies: " + str(wallet.total_coins) + "/" + str(wallet.max_coins), True, (0, 0, 0))
         window.blit(cointext, (10, 755))
+        wallet.update()
 
         for bear in self.bears: # check collisions
             if bear.health > 0:
@@ -152,6 +168,9 @@ class Level_01_State:
                             bear.atkunit = True
                         if bear.health <= 0:
                             bear.speed = -8
+                            if not bear.is_dead:
+                                wallet.total_coins += 10
+                                bear.is_dead = True
         for whale in self.whales:
             if whale.health > 0:
                 whale.speed = whale.maxspeed
@@ -176,13 +195,17 @@ class Level_01_State:
         self.bears.draw(window)
         # victory or defeat
         if self.tower1.sprite.health == 0:
-            text = font.render("VICTORY!", True, (0, 0, 0))
-            text_rect = self.tower1.sprite.image.get_rect(topleft=(600, 400))
-            window.blit(text, text_rect)
+            for bear in self.bears:
+                bear.kill()
+            Vict.draw(window)
+            Okay.draw(window)
+            Okay.update(pygame.mouse.get_pos())
         if self.tower2.sprite.health == 0:
-            text = font.render("DEFEAT!", True, (0, 0, 0))
-            text_rect = self.tower1.sprite.image.get_rect(topleft=(600, 400))
-            window.blit(text, text_rect)
+            for whale in self.whales:
+                whale.kill()
+            Def.draw(window)
+            Okay.draw(window)
+            Okay.update(pygame.mouse.get_pos())
 
 def stop(units,tower):
     for units in pygame.sprite.spritecollide(tower.sprite, units, False):
@@ -190,9 +213,9 @@ def stop(units,tower):
             units.speed = 0
             units.atktower = True
 
-Level1 = Level_01_State(viewport,wallet)
 running = True
-while running:
+Level1 = Level_01_State(viewport,wallet,running)
+while Level1.running:
     #event handler
     Level1.handle_events()
     # Render the background image
