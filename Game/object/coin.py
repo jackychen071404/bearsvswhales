4f24e53ee585f52b995b10df17afd71d02e015b4
Baby Money <108594877+jackychen071404@users.noreply.github.com>
2023-07-08 17:17:52 -0400

    Delete Game directory

--- Game/object/coin.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-class Coin(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.value = 1
-        self.rect = pygame.Rect(x, y, 10, 10)
-        self.monies = 0
